3673a3c7fac76a98a77e09c3d6544a8a66d7ba2e
mas250 <mataaronshaw@gmail.com>
2019-11-15 11:38:18 +0000

    Add project files.

---
+++ BugTracker/Models/BugContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace BugTracker.Models
+{
+    public class BugContext : DbContext
+    {
+        public  BugContext() : base()
+        {
+
+        }
+    public DbSet<Bug> Bugs { get; set; }
+
+        public System.Data.Entity.DbSet<BugTracker> Tables { get; set; }
+    }
+}
